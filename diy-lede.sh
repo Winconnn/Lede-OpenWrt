52d4716526509e05865a38aa37d2e966ee75a464
Winconnn <111964015+Winconnn@users.noreply.github.com>
2023-06-22 16:14:12 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd openwrt
-rm -rf package/lean/luci-theme-argon
+
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # 说明：
@@ -44,8 +43,10 @@ git clone -b master https://github.com/vernesong/OpenClash  #openclash出国软�
 #git clone https://github.com/jerrykuku/node-request.git package/node-request  #京东签到依赖
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus  #luci-app-jd-dailybonus[京东签到]
 
+cd openwrt
+rm -rf package/lean/luci-theme-argon
 
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #argon-主题
-#全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
-#增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
-#增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #argon-主题
+全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
+增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
+增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
